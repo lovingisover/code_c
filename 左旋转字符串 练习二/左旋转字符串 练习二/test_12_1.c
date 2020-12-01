1a8f3c2c13cd60bfa3fd9aca8f357f54c852484e
MIDwan <1846074273@qq.com>
2020-12-01 22:22:17 +0800

    左旋转字符串  二练

---
+++ 左旋转字符串 练习二/左旋转字符串 练习二/test_12_1.c
@@ -0,0 +1,21 @@
+﻿char* reverseleftwords(char *s, int n)
+{
+	if (s == NULL || n == 0)
+	{
+		return NULL;
+	}
+	int i = 0;
+	i = strlen(s);
+	char *s1 = (char*)malloc(sizeof(char)*(i + 1));
+	int j = 0;
+	for (j = 0; j < i - n; ++j)
+	{
+		s1[j] = s[n + j];
+	}﻿
+	for (int k = 0; k < n; ++k)
+	{
+		s1[i - n + k] = s[k];
+	}
+	s[i] = '\0';
+	return s1;
+}
